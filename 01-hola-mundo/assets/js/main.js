01eaa4392db6072ad897df769752baab0edc865c
Daniel Miranda <dante789ki@gmail.com>
2021-04-14 23:11:56 -0400

    [C1] descarga de vue en local

--- 01-hola-mundo/assets/js/main.js
+++ 01-hola-mundo/assets/js/main.js
@@ -1,1 +1,18 @@
-alert(" Hola Mundo desde el fichero en JavaScript 😎")
+// alert(" Hola Mundo desde el fichero en JavaScript 😎")
+const { createApp, ref } = Vue
+const App = {
+  setup() {
+    const name = ref("Daniel")
+    const count = ref(0)
+    const increment = () => count.value++
+    return { name, increment, count }
+  },
+  template: /* html */ `
+        <div>
+        <p>{{ count }}</p>
+            <button @click="increment">Incrementar</button>
+        <p>{{ name }}</p>
+        </div>
+    `
+}
+createApp(App).mount("#app")
