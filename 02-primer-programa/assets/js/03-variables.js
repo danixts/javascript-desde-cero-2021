580ec979b0786c0cdab6413681b12dfe383bfb60
Daniel Miranda <dante789ki@gmail.com>
2021-04-15 22:54:33 -0400

    [C2] uso correcto de las variables

---
+++ 02-primer-programa/assets/js/03-variables.js
@@ -0,0 +1,58 @@
+"use strict"
+
+let fruta = "uva"
+// console.log(fruta)
+fruta = "manzana"
+
+console.log(fruta)
+
+const anime = "Bleach"
+
+// anime = "Naruto"
+
+console.log(anime)
+
+// let nombre = "Daniel"
+// let apellido = "Miranda"
+
+/* let nombre = "Daniel",
+    apellido = "Miranda",
+    edad = 26 */
+
+const nombre = "Daniel",
+  apellido = "Miranda"
+
+// const nombre2 = "Daniel"
+// const formula-matematica3 = "formula"
+const formula_matematica = "formula"
+const $ = "Dolar"
+const _ = "Guion"
+
+// no recomendable
+// const delete = "delete"
+
+const PI = 3.1416
+
+// const express = require('express')
+
+const COLOR_GREEN = "green" // color primario
+
+console.log(Math.PI)
+
+{
+  let celular = "Sony"
+}
+
+let celular = "Huawei"
+
+// let celular = "Sony";
+
+console.log(celular)
+
+/* {
+    var celular = "Sony"
+}
+
+celular = "Huawei"
+
+console.log(celular) */
