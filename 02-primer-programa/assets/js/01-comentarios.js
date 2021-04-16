d57ea6681a27077f6e6d07bf832a55f29da4c7dc
Daniel Miranda <dante789ki@gmail.com>
2021-04-15 22:53:39 -0400

    [C2] comentarios

---
+++ 02-primer-programa/assets/js/01-comentarios.js
@@ -0,0 +1,22 @@
+/* alert("Clase 2 🚀");
+
+alert("Mensaje 🚀");
+
+// [1,2,3].forEach(alert)
+
+alert("Mensaje 🚀");
+
+*/
+
+/* Comentario en bloque */
+
+/**
+ *
+ * @param {string} msg
+ * @return {string}
+ */
+function saludo(msg) {
+  return msg
+}
+
+alert("Mensaje 🚀")
