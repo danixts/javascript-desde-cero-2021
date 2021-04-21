449d002a7f2333284d50c0d1ef13af5d8125f8ff
Daniel Miranda <dante789ki@gmail.com>
2021-04-20 23:41:03 -0400

    [C4] operador de comparacion

---
+++ 02-primer-programa/assets/js/07-operadores-de-comparacion.js
@@ -0,0 +1,34 @@
+const a = 9
+const b = 10
+
+console.log("COMPARA A CON B= ", a == b)
+
+const cad1 = "hola"
+const cad2 = "hola"
+
+console.log("COMPARAR cad1 con cad2", cad1 == cad2)
+
+console.log("DIFERENTE A CON B", a != b)
+
+console.log("DIFERENTE cad1 CON cad2", cad1 != cad2)
+
+console.log("MAYOR A > B", a > b)
+console.log("MENOR A < B", a < b)
+
+console.log("MAYOR CADENAS cad1 > cad2", cad1 > cad2)
+console.log("MENOR CADENAS cad1 < cad2", cad1 < cad2)
+
+console.log("MAYOR IGUAL A >= B", a >= b)
+console.log("MENOR IGUAL A <= B", a <= b)
+
+console.log("COMPARAR CADENA CON NUMERO", "4" > 2)
+console.log("COMPARAR CADENA CON NUMERO", "02" == 2)
+
+console.log("COMPARAR CADENA CON NUMERO", "02" === 2)
+
+console.log("BOOLEANO CON STRING ==", 0 == false)
+console.log("Numero", typeof 0, "Booleano", typeof false)
+console.log("BOOLEANO CON STRING ===", 0 === false)
+
+console.log("DIFERENTE DE (!=)", "10" != 10)
+console.log("DIFERENTE DE (!==)", "10" !== 10)
